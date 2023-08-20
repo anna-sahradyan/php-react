f069e8e6e7981bc1a2a3fcca5691a8dd34e1f784
Anna <71427017+anna-sahradyan@users.noreply.github.com>
2023-08-19 20:41:19 -1200

    create form with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const App = () => {
+    return (
+        <>
+            <div className=" col-md-6 mx-auto mt-5">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+                <form>
+                    <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Your Name</label>
+                    <input type="text" className="form-control" id="name" name="name" />
+
+                </div>
+                    <div className="mb-3">
+                        <label htmlFor="mobile" className="form-label">Your Mobile</label>
+                        <input type="text" className="form-control" id="mobile" name="mobile" />
+
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="email" name={"email"}/>
+
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                            <label className="form-check-label" for="exampleCheck1">I agree all statements <a
+                                href="/"><u>Terms of service</u></a>  </label>
+                    </div>
+                    <div className={"d-flex justify-content-center"}>
+                    <button type="submit" className="btn btn-success  " id={"submit"} name={"submit"}>Submit</button>
+                    </div>
+                    <p className={"text-center text-muted mt-5 mb-0"}>Have already account ? <a href="/" className={"fw-bold text-body"}><u>Login here</u></a></p>
+                </form>
+            </div>
+        </>
+    );
+};
 
 export default App;
