1d1d003296e6a6f66f0f12c43e9927185ef45079
Anna <71427017+anna-sahradyan@users.noreply.github.com>
2023-08-21 04:53:28 -1200

    added toast from react-toastify

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React from 'react';
+import Form from "./components/Form";
+import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
     return (
         <>
-            <div className=" col-md-6 mx-auto mt-5">
-
-                <form>
-                    <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Your Name</label>
-                    <input type="text" className="form-control" id="name" name="name" />
-
-                </div>
-                    <div className="mb-3">
-                        <label htmlFor="mobile" className="form-label">Your Mobile</label>
-                        <input type="text" className="form-control" id="mobile" name="mobile" />
-
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" name={"email"}/>
-
-                    </div>
-                    <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                            <label className="form-check-label" for="exampleCheck1">I agree all statements <a
-                                href="/"><u>Terms of service</u></a>  </label>
-                    </div>
-                    <div className={"d-flex justify-content-center"}>
-                    <button type="submit" className="btn btn-success  " id={"submit"} name={"submit"}>Submit</button>
-                    </div>
-                    <p className={"text-center text-muted mt-5 mb-0"}>Have already account ? <a href="/" className={"fw-bold text-body"}><u>Login here</u></a></p>
-                </form>
-            </div>
+           <ToastContainer/>
+            <Form/>
         </>
     );
 };
---
+++ src/components/Form.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import {toast} from "react-toastify";
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        name: "",
+        mobile: ""
+
+    })
+    const {name, mobile, email} = formData;
+    const onsubmit = (e) => {
+        e.preventDefault();
+        if (name.length === 0) {
+            toast.warn("Error: Name is required", {
+                className: 'custom-toast',
+            });
+        }
+        if (email.length === 0) {
+            toast.warn("Error: Email is required", {
+                className: 'custom-toast',
+            });
+        }
+        if (mobile.length === 0) {
+            toast.warn("Error: Mobile is required", {
+                className: 'custom-toast',
+            });
+        }
+        else{
+            toast.success("Success")
+        }
+
+    }
+    const handleInputChange = (event) => {
+        const {name, value,} = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }))
+
+
+    }
+
+
+    return (
+
+        <div className=" col-md-6 mx-auto mt-5">
+
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Your Name</label>
+                    <input type="text" className="form-control" id="name" name="name" onChange={handleInputChange}
+                           value={name}/>
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="mobile" className="form-label">Your Mobile</label>
+                    <input type="text" className="form-control" id="mobile" name="mobile" onChange={handleInputChange}
+                           value={mobile}/>
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name={"email"} onChange={handleInputChange}
+                           value={email}/>
+
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">I agree all statements <a
+                        href="/"><u>Terms of service</u></a> </label>
+                </div>
+                <div className={"d-flex justify-content-center"}>
+                    <button type="submit" className="btn btn-success  " id={"submit"} name={"submit"}
+                            onClick={onsubmit}>Submit
+                    </button>
+                </div>
+                <p className={"text-center text-muted mt-5 mb-0"}>Have already account ? <a href="/"
+                                                                                            className={"fw-bold text-body"}><u>Login
+                    here</u></a></p>
+            </form>
+        </div>
+
+    );
+};
+
+export default Form;
